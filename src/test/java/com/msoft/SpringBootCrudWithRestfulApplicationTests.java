60be5f3aa740415166cd897b539900b39d7037f3
GawandeMrunal <mrunal.gawande.mg@gmail.com>
2022-08-12 07:54:54 +0530

    Prject Added to central Repository

---
+++ src/test/java/com/msoft/SpringBootCrudWithRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudWithRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
